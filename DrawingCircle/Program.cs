e829fa54edcafcf88bb979c568173ff35f3dec67
Canberk Yılmaz <64089706+Relasee@users.noreply.github.com>
2023-01-12 09:20:05 +0300

    Add files via upload

---
+++ DrawingCircle/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DrawingCircle
+{
+    class Program
+    {
+        //Drawing a Circle
+        static void DrawingCircle(int n)
+        {
+
+        for (int y = -n; y <= n; y++)
+        {
+            for (int x = -n; x <= n; x++)
+            {
+                if (x * x + y * y <= n * n)
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        }
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Enter a radius :");
+           int n = int.Parse(Console.ReadLine());
+           DrawingCircle(n);
+        }
+    }
+}
