9cd3fc45f009ba39844b1f4d573578e86ab8ff1b
Canberk Yılmaz <64089706+Relasee@users.noreply.github.com>
2023-01-12 12:45:16 +0300

    Add files via upload

---
+++ Reverse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("write a sentence: ");
+            string sentence = Console.ReadLine();
+            string[] words = sentence.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                char[] characters = words[i].ToCharArray();
+                Array.Reverse(characters);
+                words[i] = new string(characters);
+            }
+            string reversed = string.Join(" ", words);
+            Console.WriteLine(reversed);
+        }
+    }
+}
