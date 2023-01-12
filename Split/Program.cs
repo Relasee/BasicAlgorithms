9164786946e1492b58629fed744cac5dffa43035
Canberk Yılmaz <64089706+Relasee@users.noreply.github.com>
2023-01-12 12:46:10 +0300

    Add files via upload

---
+++ Split/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Split
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Write a word and number : (with ,)");
+           string s = Console.ReadLine();
+           string[] arr = s.Split(',');
+           for(int i=0;i<arr.Length;i+=2){
+                Console.Write(arr[i]+" ");
+           }
+        }
+    }
+}
